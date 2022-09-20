8ad6180dbb36728245ef53cc6f63acc4df5f18dc
teyeaddo <addorichmondteye@gmail.com>
2022-09-20 10:31:21 +0000

    Adding new files on Pointers and Arrays

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+* reset_to_98 - point to an integer and reset that value to 98
+* @n: a pointer to an int that will be updated
+*Return: 0
+*/
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+A new folder containing my tasks on Pointers, Arrays and Strings
